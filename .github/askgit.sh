d0a4f02098a3d3f6038f68f3313d0ab70f9c8696
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2020-03-29 16:08:29 +0200

    [pertoldi@lxpertoldi.pd.infn.it] Sun Mar 29 16:08:29 CEST 2020

---
+++ .github/askgit.sh
@@ -0,0 +1,34 @@
+update_progress_plot() {
+    \cd ..
+    echo `compose_line` >> .github/wc.dat
+    \cd - > /dev/null
+
+    xelatex -output-directory=.auxtex progress.tex
+    cp .auxtex/progress.pdf .
+}
+
+generate_all() {
+    \cd ..
+    for commit in `git rev-list --all`; do
+        rm .github/wc.dat
+        echo `compose_line_from_commit $commit` >> .github/wc.dat
+    done
+    \cd - > /dev/null
+}
+
+compose_line() {
+    echo `date +'%Y-%m-%d %H:%M:%S'`, \
+         `cat $(find src      | grep -E '.*\.(bib|tex|asy|C)') | wc -w`, \
+         `cat $(find src/chap | grep -E '.*\.(tex)')           | wc -w`, \
+         `cat $(find src/img  | grep -E '.*\.(tex|asy|C)')     | wc -w`, \
+         `cat $(find src/bib  | grep -E '.*\.(bib)')           | wc -w`
+}
+
+compose_line_from_commit() {
+    ts=`git --no-pager log -n 1 --pretty=%at $1`
+    echo `date -d @$ts +'%Y-%m-%d %H:%M:%S'`, \
+         `git archive $1 -- $(git ls-tree -r --name-only $1             | grep -E '.*\.(tex|bib|asy|C)') | tar -x -O | wc -w`, \
+         `git archive $1 -- $(git ls-tree -r --name-only $1 -- src/chap | grep -E '.*\.tex')             | tar -x -O | wc -w`, \
+         `git archive $1 -- $(git ls-tree -r --name-only $1 -- src/img  | grep -E '.*\.(tex|asy|C)')     | tar -x -O | wc -w`, \
+         `git archive $1 -- $(git ls-tree -r --name-only $1 -- src/bib  | grep -E '.*\.bib')             | tar -x -O | wc -w`
+}
